4f43af830670ea79975172b770cf75ded10b8f59
Pierre Durand <pierredurand@gmail.com>
2023-01-22 01:03:37 +0100

    add tests

--- di_test.go
+++ di_test.go
@@ -2,10 +2,150 @@
 package di
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"testing"
 )
+
+func Test(t *testing.T) {
+	c := new(Container)
+	builderCallCount := 0
+	Set(c, "", func(c *Container) (*serviceA, Close, error) {
+		builderCallCount++
+		return &serviceA{}, nil, nil
+	})
+	sa, err := Get[*serviceA](c, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if sa == nil {
+		t.Fatal("nil service")
+	}
+	sa, err = Get[*serviceA](c, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if sa == nil {
+		t.Fatal("nil service")
+	}
+	if builderCallCount != 1 {
+		t.Fatal("builder called more than once")
+	}
+}
+
+func TestSetPanicAlreadySet(t *testing.T) {
+	c := new(Container)
+	Set(c, "", func(c *Container) (*serviceA, Close, error) {
+		return &serviceA{}, nil, nil
+	})
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Fatal("no panic")
+		}
+	}()
+	Set(c, "", func(c *Container) (*serviceA, Close, error) {
+		return &serviceA{}, nil, nil
+	})
+}
+
+func TestGetErrorNotRegistered(t *testing.T) {
+	c := new(Container)
+	_, err := Get[*serviceA](c, "")
+	if err == nil {
+		t.Fatal("no error")
+	}
+}
+
+func TestGetErrorType(t *testing.T) {
+	c := new(Container)
+	Set(c, "test", func(c *Container) (*serviceA, Close, error) {
+		return &serviceA{}, nil, nil
+	})
+	_, err := Get[*serviceB](c, "test")
+	if err == nil {
+		t.Fatal("no error")
+	}
+}
+
+func TestGetErrorBuilder(t *testing.T) {
+	c := new(Container)
+	Set(c, "", func(c *Container) (*serviceA, Close, error) {
+		return nil, nil, errors.New("error")
+	})
+	_, err := Get[*serviceA](c, "")
+	if err == nil {
+		t.Fatal("no error")
+	}
+}
+
+func TestClose(t *testing.T) {
+	c := new(Container)
+	var closeServiceCalled bool
+	Set(c, "", func(c *Container) (*serviceA, Close, error) {
+		return &serviceA{}, func() error {
+			closeServiceCalled = true
+			return nil
+		}, nil
+	})
+	_, err := Get[*serviceA](c, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	c.Close(func(err error) {
+		t.Fatal(err)
+	})
+	if !closeServiceCalled {
+		t.Fatal("close service not called")
+	}
+}
+
+func TestCloseNil(t *testing.T) {
+	c := new(Container)
+	Set(c, "", func(c *Container) (*serviceA, Close, error) {
+		return &serviceA{}, nil, nil
+	})
+	_, err := Get[*serviceA](c, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	c.Close(func(err error) {
+		t.Fatal(err)
+	})
+}
+
+func TestCloseError(t *testing.T) {
+	c := new(Container)
+	Set(c, "", func(c *Container) (*serviceA, Close, error) {
+		return &serviceA{}, func() error {
+			return errors.New("error")
+		}, nil
+	})
+	_, err := Get[*serviceA](c, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	c.Close(func(err error) {
+		if err == nil {
+			t.Fatal("no error")
+		}
+	})
+}
+
+func TestMust(t *testing.T) {
+	Must("", nil)
+}
+
+func TestMustPanic(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Fatal("no panic")
+		}
+	}()
+	Must("", errors.New("error"))
+}
 
 func Example() {
 	// New container.
