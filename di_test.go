5037bbb15ce44097b2d9d14a584f83fd1c8b4b24
Pierre Durand <pierredurand@gmail.com>
2024-08-03 12:33:30 +0200

    improve example

--- di_test.go
+++ di_test.go
@@ -16,40 +16,57 @@ func Example() {
 	ctn := new(Container)
 	defer ctn.Close(ctx)
 	MustSet(ctn, "", func(ctx context.Context, ctn *Container) (*serviceA, Close, error) {
-		return &serviceA{}, nil, nil
+		fmt.Println("start build A")
+		defer fmt.Println("end build A")
+		return &serviceA{
+			b: MustGet[*serviceB](ctx, ctn, "").doB,
+			c: MustGet[*serviceC](ctx, ctn, "").doC,
+		}, nil, nil
 	})
 	MustSet(ctn, "", func(ctx context.Context, ctn *Container) (*serviceB, Close, error) {
-		sb := &serviceB{
-			sa: MustGet[*serviceA](ctx, ctn, "").DoA,
-		}
+		fmt.Println("start build B")
+		defer fmt.Println("end build B")
+		sb := &serviceB{}
 		return sb, sb.close, nil
 	})
 	MustSet(ctn, "", func(ctx context.Context, ctn *Container) (*serviceC, Close, error) {
-		return &serviceC{
-			sb: MustGet[*serviceB](ctx, ctn, "").DoB,
-		}, nil, nil
+		fmt.Println("start build C")
+		defer fmt.Println("end build C")
+		return &serviceC{}, nil, nil
 	})
-	sc := MustGet[*serviceC](ctx, ctn, "")
-	sc.DoC()
+	fmt.Println("configured")
+	sa := MustGet[*serviceA](ctx, ctn, "")
+	fmt.Println("initialized")
+	sa.doA()
 	// Output:
-	// do A
+	// configured
+	// start build A
+	// start build B
+	// end build B
+	// start build C
+	// end build C
+	// end build A
+	// initialized
 	// do B
+	// do A
 	// do C
 	// close B
 }
 
-type serviceA struct{}
+type serviceA struct {
+	b func()
+	c func()
+}
 
-func (sa *serviceA) DoA() {
+func (sa *serviceA) doA() {
+	sa.b()
 	fmt.Println("do A")
+	sa.c()
 }
 
-type serviceB struct {
-	sa func()
-}
+type serviceB struct{}
 
-func (sb *serviceB) DoB() {
-	sb.sa()
+func (sb *serviceB) doB() {
 	fmt.Println("do B")
 }
 
@@ -58,12 +75,9 @@ func (sb *serviceB) close(ctx context.Context) error {
 	return nil
 }
 
-type serviceC struct {
-	sb func()
-}
+type serviceC struct{}
 
-func (sc *serviceC) DoC() {
-	sc.sb()
+func (sc *serviceC) doC() {
 	fmt.Println("do C")
 }
 
