c7db64f64a4ab090443718bb9c4dd49a75dd019a
Pierre Durand <pierredurand@gmail.com>
2024-08-05 00:40:27 +0200

    extract builder functions in Example() in order to make the example simpler to read

--- di_example_test.go
+++ di_example_test.go
@@ -9,25 +9,9 @@ func Example() {
 	ctx := context.Background()
 	ctn := new(Container)
 	defer ctn.Close(ctx)
-	MustSet(ctn, "", func(ctx context.Context, ctn *Container) (*serviceA, Close, error) {
-		fmt.Println("start build A")
-		defer fmt.Println("end build A")
-		return &serviceA{
-			b: MustGet[*serviceB](ctx, ctn, "").doB,
-			c: MustGet[*serviceC](ctx, ctn, "").doC,
-		}, nil, nil
-	})
-	MustSet(ctn, "", func(ctx context.Context, ctn *Container) (*serviceB, Close, error) {
-		fmt.Println("start build B")
-		defer fmt.Println("end build B")
-		sb := &serviceB{}
-		return sb, sb.close, nil
-	})
-	MustSet(ctn, "", func(ctx context.Context, ctn *Container) (*serviceC, Close, error) {
-		fmt.Println("start build C")
-		defer fmt.Println("end build C")
-		return &serviceC{}, nil, nil
-	})
+	MustSet(ctn, "", buildServiceA)
+	MustSet(ctn, "", buildServiceB)
+	MustSet(ctn, "", buildServiceC)
 	fmt.Println("configured")
 	sa := MustGet[*serviceA](ctx, ctn, "")
 	fmt.Println("initialized")
@@ -52,6 +36,15 @@ type serviceA struct {
 	c func()
 }
 
+func buildServiceA(ctx context.Context, ctn *Container) (*serviceA, Close, error) {
+	fmt.Println("start build A")
+	defer fmt.Println("end build A")
+	return &serviceA{
+		b: MustGet[*serviceB](ctx, ctn, "").doB,
+		c: MustGet[*serviceC](ctx, ctn, "").doC,
+	}, nil, nil
+}
+
 func (sa *serviceA) doA() {
 	sa.b()
 	fmt.Println("do A")
@@ -59,6 +52,13 @@ func (sa *serviceA) doA() {
 }
 
 type serviceB struct{}
+
+func buildServiceB(ctx context.Context, ctn *Container) (*serviceB, Close, error) {
+	fmt.Println("start build B")
+	defer fmt.Println("end build B")
+	sb := &serviceB{}
+	return sb, sb.close, nil
+}
 
 func (sb *serviceB) doB() {
 	fmt.Println("do B")
@@ -70,6 +70,12 @@ func (sb *serviceB) close(ctx context.Context) error {
 }
 
 type serviceC struct{}
+
+func buildServiceC(ctx context.Context, ctn *Container) (*serviceC, Close, error) {
+	fmt.Println("start build C")
+	defer fmt.Println("end build C")
+	return &serviceC{}, nil, nil
+}
 
 func (sc *serviceC) doC() {
 	fmt.Println("do C")
